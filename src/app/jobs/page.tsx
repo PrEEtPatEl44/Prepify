480362a28968a02212989da70489e289bc3b1254
PrEEtPatEl44 <preetp4404@gmail.com>
2025-09-25 15:43:44 -0400

    added loading skeleton

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -5,20 +5,25 @@ import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
 import { getAllJobs, createJob, getAllColumns } from "@/lib/clients";
 import { type Column, type Job } from "@/types/jobs";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Example = dynamic(() => import("@/components/kanban"), { ssr: false });
 
 const Page = () => {
   const [jobs, setJobs] = useState<Job[]>([]);
   const [columns, setColumns] = useState<Column[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
+  // Fetch jobs and columns on component mount
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       const columnsData = await getAllColumns();
       const jobsData = await getAllJobs();
       console.log("Fetched jobs:", jobsData);
       setColumns(columnsData);
       setJobs(jobsData);
+      setIsLoading(false);
     };
     fetchData();
   }, []);
@@ -64,6 +69,21 @@ const Page = () => {
 
     setJobs((prevJobs) => [...prevJobs, newJob]);
   };
+
+  if (isLoading) {
+    return (
+      <div className="h-screen flex flex-col w-full">
+        <Skeleton className="h-12 mt-6 px-1 max-w-[95%] rounded-xl bg-gray-200 mb-2" />
+
+        <div className="flex flex-1 gap-2">
+          <Skeleton className="flex-1 rounded-lg bg-gray-200" />
+          <Skeleton className="flex-1 rounded-lg bg-gray-200" />
+          <Skeleton className="flex-1 rounded-lg bg-gray-200" />
+          <Skeleton className="flex-1 rounded-lg bg-gray-200" />
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="h-screen flex flex-col overflow-hidden">
