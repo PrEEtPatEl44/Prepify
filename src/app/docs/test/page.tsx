c9dc0b287f59b20012a816ab7ad9338c4407e2c6
PrEEtPatEl44 <preetp4404@gmail.com>
2025-10-07 23:27:57 -0400

    removed commented code from test page and added placeholder

--- src/app/docs/test/page.tsx
+++ src/app/docs/test/page.tsx
@@ -1,13 +1,7 @@
-// "use client";
+import React from "react";
 
-// import React from "react";
-// import Viewer from "@/components/viewer";
+const Page = () => {
+  return <div>page</div>;
+};
 
-// export default function FileUploadTest() {
-//   return (
-//     <Viewer
-//       fileUrl="https://uivqnphnsjlcatjesqug.supabase.co/storage/v1/object/sign/documents/0e5d3f3b-109f-4a37-8a0d-9ae60992caa7/resumes/1759877019643_A4-booklet-landscape_en.pdf?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9jMDJjZWIwNS1mODRmLTRjYmQtOWMzNi1mYWU2MTMwMzkyNDMiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJkb2N1bWVudHMvMGU1ZDNmM2ItMTA5Zi00YTM3LThhMGQtOWFlNjA5OTJjYWE3L3Jlc3VtZXMvMTc1OTg3NzAxOTY0M19BNC1ib29rbGV0LWxhbmRzY2FwZV9lbi5wZGYiLCJpYXQiOjE3NTk4NzcwMzgsImV4cCI6MTc2MDQ4MTgzOH0.KmLGvaH4KOSusl-BArHMQYXqrduOC6FbQO1kbpD9Hwk"
-//       fileName="Test PDF Viewer"
-//     />
-//   );
-// }
+export default Page;
