f7d38317592dbe8a5085acd900eba13bdca42b99
PrEEtPatEl44 <preetp4404@gmail.com>
2026-02-13 23:17:32 -0500

    updated schema

--- src/db/relations.ts
+++ src/db/relations.ts
@@ -1,37 +1,29 @@
 import { relations } from "drizzle-orm/relations";
-import {
-  columns,
-  coverLetters,
-  resumes,
-  jobApplications,
-} from "./schema";
+import { columns, jobApplications, coverLetters, resumes } from "./schema";
 
-export const columnsRelations = relations(columns, ({ many }) => ({
-  jobApplications: many(jobApplications),
+export const jobApplicationsRelations = relations(jobApplications, ({one}) => ({
+	column: one(columns, {
+		fields: [jobApplications.columnId],
+		references: [columns.id]
+	}),
+	coverLetter: one(coverLetters, {
+		fields: [jobApplications.coverLetterId],
+		references: [coverLetters.id]
+	}),
+	resume: one(resumes, {
+		fields: [jobApplications.resumeId],
+		references: [resumes.id]
+	}),
 }));
 
-export const coverLettersRelations = relations(coverLetters, ({ many }) => ({
-  jobApplications: many(jobApplications),
+export const columnsRelations = relations(columns, ({many}) => ({
+	jobApplications: many(jobApplications),
 }));
 
-export const resumesRelations = relations(resumes, ({ many }) => ({
-  jobApplications: many(jobApplications),
+export const coverLettersRelations = relations(coverLetters, ({many}) => ({
+	jobApplications: many(jobApplications),
 }));
 
-export const jobApplicationsRelations = relations(
-  jobApplications,
-  ({ one }) => ({
-    column: one(columns, {
-      fields: [jobApplications.columnId],
-      references: [columns.id],
-    }),
-    coverLetter: one(coverLetters, {
-      fields: [jobApplications.coverLetterId],
-      references: [coverLetters.id],
-    }),
-    resume: one(resumes, {
-      fields: [jobApplications.resumeId],
-      references: [resumes.id],
-    }),
-  })
-);
+export const resumesRelations = relations(resumes, ({many}) => ({
+	jobApplications: many(jobApplications),
+}));
